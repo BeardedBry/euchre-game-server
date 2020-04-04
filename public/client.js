b38e997e9fe9628a5b47879859c709538c6479ec
Brian Burkard <brianburkard@gmail.com>
2020-04-04 15:39:35 -0400

    playing with time

--- public/client.js
+++ public/client.js
@@ -11,7 +11,7 @@
         if(nameInput.value.length > 0){
             userName = nameInput.value;
             console.log('name set to ' + userName);
-            document.querySelector('#name-screen').style.display = 'none';
+            document.querySelector('#wrapper').style.display = 'none';
             document.querySelector('#chat-screen').style.display = 'block';
             document.body.style.backgroundColor = '#EEE';
             socket.emit('user joined', {
@@ -27,27 +27,44 @@
         var msgInput = this.querySelector('#m');
         socket.emit('chat message', {
             message: msgInput.value,
-            user: userName
+            user: userName,
+            time: new Date()
         });
         // do something
         msgInput.value = '';
     });
 
-    // Recieve user joined
+    // Receive user joined
     socket.on('user joined', function(userObj){
         var liSpan = newLi(`<span class="anouncement-user">${userObj.user}</span> has joined the chat.`,['anouncement']);
         console.log('user joined', userObj);
         messageUl.appendChild(liSpan);
     })
 
-    // Recieve Message
+    // Receive Message
     socket.on('all msg', function(msgObject){
-        //var li = document.createElement('LI');
-        //li.innerHTML = `<strong>${msgObject.user}</strong>: ${msgObject.message}`;
-        var li = newLi(`<strong>${msgObject.user}</strong>: ${msgObject.message}`);
+        var time = handleTime(msgObject.time);
+        var li = newLi(`<strong>${msgObject.user}</strong>: ${msgObject.message} <span class="message-time">${time}</span>`);
         messageUl.appendChild(li);
     });
 
+
+    // return formatted time
+    function handleTime(date){
+        var today = new Date(date);
+        var hour = today.getHours();
+        var minutes = today.getMinutes();
+
+        var meridiem = 'pm';
+        //var noon = hour > 11 ? 'pm' : 'am';
+        if (hour > 12){
+            hour = hour - 12;
+        }else{
+            meridiem = 'am';
+        }
+        return (hour + ":" + minutes + ' ' + meridiem);
+        //return [hour, minutes, meridiem];
+    }
 
     // UI
 
